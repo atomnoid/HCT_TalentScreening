3df381d9a9957d7cdb322b2389e53eff08ff8927
atomnoid <aayushs2580@gmail.com>
2026-07-01 03:17:24 +0530

    added routing code

--- HCT-talentscreening/src/App.jsx
+++ HCT-talentscreening/src/App.jsx
@@ -1,7 +1,21 @@
-import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ApplicantDashboard from "./pages/ApplicantDashboard";
+import HRDashboard from "./pages/HRDashboard";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/applicant" element={<ApplicantDashboard />} />
+        <Route path="/hr" element={<HRDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
