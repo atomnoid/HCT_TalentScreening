599077bb308df054736626ae9ebc01ed548ac81a
atomnoid <aayushs2580@gmail.com>
2026-07-01 03:33:54 +0530

    added usestate

--- HCT-talentscreening/src/pages/Register.jsx
+++ HCT-talentscreening/src/pages/Register.jsx
@@ -1,1 +1,10 @@
 import { Link } from "react-router-dom";
+
+const [formData, setFormData] = useState({
+  fullName: "",
+  email: "",
+  phone: "",
+  password: "",
+  role: "",
+  portfolio: "",
+});
