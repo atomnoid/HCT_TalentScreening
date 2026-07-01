1f10be705f89c378cd7cd4637e3783225f01557a
atomnoid <aayushs2580@gmail.com>
2026-07-01 13:24:12 +0530

    added a new component for navigate

--- HCT-talentscreening/src/pages/Register.jsx
+++ HCT-talentscreening/src/pages/Register.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function Register() {
   const [roles, setRoles] = useState([]);
+  const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
     fullName: "",
