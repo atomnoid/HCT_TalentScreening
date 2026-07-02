8c6b2126ecb32ceb84574e7420a192ae04d61c62
atomnoid <aayushs2580@gmail.com>
2026-07-02 12:18:49 +0530

    added a comment

--- HCT-talentscreening/src/services/questionService.js
+++ HCT-talentscreening/src/services/questionService.js
@@ -43,7 +43,7 @@ export async function getApplicantProfile() {
 
   return profile;
 }
-
+// crud -
 export async function getQuestionsByRole(roleId) {
   const { data, error } = await supabase
     .from("questions")
