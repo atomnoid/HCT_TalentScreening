70f1e0cc43f8ce9dca65eaec1dd0548118650118
atomnoid <aayushs2580@gmail.com>
2026-07-02 18:44:20 +0530

    imported quiz and results

--- HCT-talentscreening/src/App.jsx
+++ HCT-talentscreening/src/App.jsx
@@ -4,6 +4,8 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ApplicantDashboard from "./pages/ApplicantDashboard";
 import HRDashboard from "./pages/HRDashboard";
+import Quiz from "./pages/Quiz";
+import Results from "./pages/Results";
 
 function App() {
   return (
@@ -11,7 +13,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
         <Route path="/applicant" element={<ApplicantDashboard />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/results" element={<Results />} />
+
         <Route path="/hr" element={<HRDashboard />} />
       </Routes>
     </BrowserRouter>
