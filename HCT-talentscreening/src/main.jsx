c49fc12ca19d7b91633b3eb416e6c9200388d3ae
atomnoid <aayushs2580@gmail.com>
2026-07-15 01:25:20 +0530

    feat: wrap App component with ErrorBoundary for improved error handling

--- HCT-talentscreening/src/main.jsx
+++ HCT-talentscreening/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import './index.css'
 import App from './App.jsx'
+import ErrorBoundary from './components/ErrorBoundary.jsx'
 
 // Application entry point
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
     <Toaster
       position="top-right"
       toastOptions={{
